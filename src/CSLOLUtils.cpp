95a3538a32755564cf1816ec0317ea03283320fc
Morilli <35152647+Morilli@users.noreply.github.com>
2023-09-30 21:33:49 +0200

    worked but test again without

--- src/CSLOLUtils.cpp
+++ src/CSLOLUtils.cpp
@@ -257,7 +257,7 @@ void CSLOLUtils::relaunchAdmin(int argc, char *argv[]) {
     char* args[] = { strdup("admin"), NULL };
     FILE* pipe = NULL;
 
-    QDir::setCurrent(QFileInfo(path).dir().path());
+    // QDir::setCurrent(QFileInfo(path).dir().path());
     OSStatus execStatus = AuthorizationExecuteWithPrivileges(authorizationRef, path, kAuthorizationFlagDefaults, args, &pipe);
     if (execStatus != errAuthorizationSuccess) {
         fprintf(stderr, "Failed to exec auth: %x\n", execStatus);
